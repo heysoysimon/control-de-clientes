98cdaa9418879b36087ea7935b971a7433c5c715
simon <sipero2013@gmail.com>
2022-08-25 18:09:02 -0400

    validacion añadida y json server

---
+++ src/components/Alerta.jsx
@@ -0,0 +1,14 @@
+
+
+const Alerta = ({children}) => {
+  return (
+    <div className='
+    text-white bg-red-600 
+    text-center my-4 
+    font-bold p-3 uppercase'>
+    {children}
+    </div> 
+  )
+}
+
+export default Alerta
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,7 +1,51 @@
 import React from 'react'
-import { Formik, Form , Field } from 'formik'
+import { Formik, Form , Field} from 'formik'
+import * as yup from 'yup'
+import Alerta from './Alerta'
 
 const Formulario = () => {
+
+  const nuevoClienteSchema = yup.object().shape({
+  /* validaciones */
+    nombre: yup.string()
+    .min(3, "el nombre es muy corto" )
+    .max(20, "el nombre es muy largo" )
+    .required('este campo es obligatorio'),
+
+    empresa: yup.string()
+    .required("el nombre de la empresa es obligatorio"),
+
+    email: yup.string()
+    .email("introdusca un email valido ")
+      .required('el Email es obligatorio'),
+
+      telefono: yup.number()
+      .positive("numero no valuido")
+      .integer("numero no valido")
+      .typeError("el numero no es valido")
+  })
+/* por aqui se envia los atos a un backeng */
+  const  handletSubmit = async (valores) =>{
+/*       console.log(valores); */
+          try{
+            const url = 'http://localhost:3000/clientes'
+
+            const respuesta = await fetch(url,{
+              method: 'POST',
+              body: JSON.stringify(valores),
+              headers: {
+                'Content-Type': 'application/json'
+            }
+            })
+
+            console.log(respuesta)
+            const resultado = respuesta.json()
+            console.log(resultado)
+            
+          }catch(error) {
+            console.log(error)
+          }
+  }
   return (
     <div className='bg-white mt-10 px-5 py-10 rounded-md shadow-md
     md:w-3/4 mx-auto'>
@@ -11,7 +55,25 @@ const Formulario = () => {
       uppercase
       text-center'> Agregar cliente</h1>
 
-      <Formik>
+      <Formik
+          initialValues={{
+            nombre: '',
+            empresa: '',
+            email: '',
+            telefono:'',
+            notas: ''
+          }}
+            /* valores de arrida  */
+          onSubmit={(values) =>{
+                handletSubmit(values)
+          }}
+
+          validationSchema={nuevoClienteSchema}
+      >
+
+        {({errors,touched}) => {
+          console.log(touched) 
+          return(
             <Form className='mt-10'>
 
                <div className='mb-4'>
@@ -26,10 +88,15 @@ const Formulario = () => {
                type="text"
                placeholder="nombre del cliente"
                className="mt-2 w-full p-3 bg-gray-50"
+               name="nombre"
                />
-
                </div>
 
+               {errors.nombre && touched.nombre ?(
+                  <Alerta>{errors.nombre}</Alerta>
+
+              ): null}
+                
                <div className='mb-4'>
                 <label 
                 className='text-gray-800'
@@ -40,7 +107,14 @@ const Formulario = () => {
                type="text"
                placeholder="nombre de la empresa"
                className="mt-2 w-full p-3 bg-gray-50"
+               name="empresa"
                />
+
+                
+               {errors.empresa && touched.empresa ?(
+                  <Alerta>{errors.empresa}</Alerta>
+
+              ): null}
 
                </div>
 
@@ -54,8 +128,13 @@ const Formulario = () => {
                type="email"
                placeholder="email del cliente "
                className="mt-2 w-full p-3 bg-gray-50"
+               name="email"
                />
 
+          {errors.email && touched.email ?(
+                  <Alerta>{errors.email}</Alerta>
+
+              ): null}
                </div>
 
 
@@ -69,8 +148,14 @@ const Formulario = () => {
                type="telf"
                placeholder="numero del cliente "
                className="mt-2 w-full p-3 bg-gray-50"
+               name="telefono"
                />
 
+
+               {errors.telefono && touched.telefono ?(
+                  <Alerta>{errors.telefono}</Alerta>
+
+              ): null}
                </div>
 
 
@@ -87,6 +172,7 @@ const Formulario = () => {
                as="textarea"
                placeholder="notas del cliente  "
                className="mt-2 w-full p-3 bg-gray-50"
+               name="notas"
                />
 
                </div>
@@ -99,7 +185,9 @@ const Formulario = () => {
                />
 
             </Form>
+               )}}
       </Formik>
+      
     </div>
   )
 }
